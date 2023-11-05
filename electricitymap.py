12eb365bb563b755e0d1df7d3672c9d4fe068c2d
root <root@Fidelis>
2023-11-05 17:01:48 +0100

    data collection noaa & openmeteo

--- electricitymap.py
+++ electricitymap.py
@@ -1,44 +1,33 @@
 import requests
 
-def fetch_electricity_map_data(zone="FR"):
+def fetch_power_breakdown_data(zone="MA"):
     """
-    Récupère les données de l'API ElectricityMap pour une zone spécifique.
+    Récupère les données de décomposition de la puissance de l'API ElectricityMap pour une zone spécifique.
     
-    :param zone: La zone pour laquelle vous voulez obtenir les données (par exemple, "FR" pour la France).
-    :return: Un dictionnaire contenant les données de consommation, d'émission de CO2, et de production d'énergie.
+    :param zone: La zone pour laquelle vous voulez obtenir les données (par exemple, "MA" pour le Maroc).
+    :return: Un dictionnaire contenant les données de décomposition de la puissance.
     """
-    base_url = "https://api.electricitymap.org/v3/zones"
-    url = f"{base_url}/{zone}"
+    base_url = "https://api-access.electricitymaps.com/free-tier/power-breakdown/latest"
+    
+    # Inclure la zone dans les paramètres de la requête
+    params = {'zone': zone}
+    
+    # Remplacez "YOUR_API_KEY" par votre clé d'API
+    api_key = "oQWn2xj45ukPWC3v1lMWJclIY43ngkKY"
 
     try:
-        response = requests.get(url)
+        # Inclure la clé d'API dans les en-têtes de la requête
+        headers = {'Authorization': f'Token {api_key}'}
+        response = requests.get(base_url, params=params, headers=headers)
         data = response.json()
         
-        if 'zoneKey' in data:
-            # Les données sont disponibles pour cette zone
-            consumption = data['consumption']['current']
-            carbon_intensity = data['carbonIntensity']['current']
-            production = data['production']
-            
-            return {
-                'zone': zone,
-                'consumption': consumption,
-                'carbon_intensity': carbon_intensity,
-                'production': production
-            }
-        else:
-            print(f"Données non disponibles pour la zone {zone}")
-            return None
+        return data
     except requests.exceptions.RequestException as e:
         print(f"Erreur lors de la requête : {e}")
         return None
 
 # Exemple d'utilisation
-zone_data = fetch_electricity_map_data("MA")
-if zone_data:
-    print("Zone:", zone_data['zone'])
-    print("Consommation:", zone_data['consumption'], "MW")
-    print("Intensité carbone:", zone_data['carbon_intensity'], "gCO2eq/kWh")
-    print("Production d'énergie:")
-    for source, value in zone_data['production'].items():
-        print(f"{source}: {value} MW")
+power_breakdown_data = fetch_power_breakdown_data("MA")
+if power_breakdown_data:
+    print("Données de décomposition de la puissance pour la zone MA:")
+    print(power_breakdown_data)
